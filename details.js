fc244e2b821b3c4ddc0eff224d69ab8192cb5e0c
walex <olawaleibrahim10@gmail.com>
2022-06-22 12:06:00 +0100

    my first task on javascript

---
+++ details.js
@@ -0,0 +1,15 @@
+// alert("ramon ibrahim olawale")
+// // Logging to console
+// console.log("This is from function 1")
+
+
+// // Changing the func1 name to function1
+// // using the function.displayname
+// func1.displayName = "function1"
+// console.log("Display name of the function"
+//         + " func1 is :", func1.displayName)
+
+
+function func1(){}
+    func1.displayName="ibrahim, Nigeria, 65"
+    console.log(func1.displayName)
